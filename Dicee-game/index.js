633995da0eb8285cc21e898dd7d60498c70a77b2
Kavindu Fernando <kavinduf2@gmail.com>
2023-11-24 15:06:54 +0530

    Shorten Js

--- Dicee-game/index.js
+++ Dicee-game/index.js
@@ -10,39 +10,44 @@ let num2
 let winner = document.querySelector(".heading")
 
 function ChangeImage1 (){
-  num1 = Math.floor(Math.random()*6);
-  let image1 = document.querySelector(".img1, src")
-  if(num1 == 0){
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png")
-  }else if(num1 == 1){
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png")
-  }else if(num1 == 2){
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png")
-  }else if(num1 == 3){
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png")
-  }else if(num1 == 4){
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png")
-  }else if(num1 == 5){
-    document.querySelector(".img1").setAttribute("src", "images/dice6.png")
-  }
+  num1 = Math.floor(Math.random()*6+1);
+  // let image1 = document.querySelector(".img1, src")
+  let image1 = document.querySelector(".img1")
+  let randomImage = "images/dice" + num1 + ".png"
+  image1.setAttribute("src", randomImage)
+  // if(num1 == 0){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice1.png")
+  // }else if(num1 == 1){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice2.png")
+  // }else if(num1 == 2){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice3.png")
+  // }else if(num1 == 3){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice4.png")
+  // }else if(num1 == 4){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice5.png")
+  // }else if(num1 == 5){
+  //   document.querySelector(".img1").setAttribute("src", "images/dice6.png")
+  // }
 }
 
 function ChangeImage2 (){
-  num2 = Math.floor(Math.random()*6);
+  num2 = Math.floor(Math.random()*6 + 1);
   let image2 = document.querySelector(".img2, src")
-  if(num2 == 0){
-    document.querySelector(".img2").setAttribute("src", "images/dice1.png")
-  }else if(num2 == 1){
-    document.querySelector(".img2").setAttribute("src", "images/dice2.png")
-  }else if(num2 == 2){
-    document.querySelector(".img2").setAttribute("src", "images/dice3.png")
-  }else if(num2 == 3){
-    document.querySelector(".img2").setAttribute("src", "images/dice4.png")
-  }else if(num2 == 4){
-    document.querySelector(".img2").setAttribute("src", "images/dice5.png")
-  }else if(num2 == 5){
-    document.querySelector(".img2").setAttribute("src", "images/dice6.png")
-  }
+  let randomImage2 = "images/dice" + num2 + ".png"
+  image2.setAttribute("src", randomImage2)
+  // if(num2 == 0){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice1.png")
+  // }else if(num2 == 1){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice2.png")
+  // }else if(num2 == 2){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice3.png")
+  // }else if(num2 == 3){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice4.png")
+  // }else if(num2 == 4){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice5.png")
+  // }else if(num2 == 5){
+  //   document.querySelector(".img2").setAttribute("src", "images/dice6.png")
+  // }
 }
 
 function GuessTheWinner(){
