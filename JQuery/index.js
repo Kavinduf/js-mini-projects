b8d34c04ee2a49e7ea73ee6bd99817b9f5d9c9bc
Kavindu Fernando <kavinduf2@gmail.com>
2023-12-05 14:11:26 +0530

    jquery

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,1 +1,11 @@
-$("h1").css("color", "red");
+// console.log($("h1").css("color"));
+
+//change styles
+// $("h1").css("color", "yellow");
+
+//add class
+// $("h1").addClass("big-title");
+$("h1").addClass("big-title margin-50");
+
+//check class
+// $("h1").hasClass("margin-50");
