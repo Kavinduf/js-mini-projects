65bd46f70af39335e255f540bc71ad19d789fbde
Kavindu Fernando <kavinduf2@gmail.com>
2023-12-06 17:33:58 +0530

    SImon game

--- Simon-Game/index.js
+++ Simon-Game/index.js
@@ -1,66 +1,129 @@
 //Game start
 
-var task = [];
-var buttonClicks = [];
-var gameStatus;
-var looping;
+// var task = [];
+// var buttonClicks = [];
+// var gameStatus;
+// var looping;
 
-if (gameStatus == "over" && looping == false) {
-	gameStatus = "start";
+// if (gameStatus == "over" && looping == false) {
+// 	gameStatus = "start";
 
-	$("body").keypress(function () {
-		looping = true;
+// 	$("body").keypress(function () {
+// 		looping = true;
 
-		$("h1").text("Level 1");
-		randomSelector();
-	});
+// 		$("h1").text("Level 1");
+// 		randomSelector();
+// 	});
+// }
+
+// function randomSelector() {
+// 	var random = Math.floor(Math.random() * 4);
+// 	task.push(random);
+
+// 	switch (random) {
+// 		case 0:
+// 			$(".green").fadeOut(100).fadeIn(100);
+// 			break;
+// 		case 1:
+// 			$(".red").fadeOut(100).fadeIn(100);
+// 			break;
+// 		case 2:
+// 			$(".yellow").fadeOut(100).fadeIn(100);
+// 			break;
+// 		case 3:
+// 			$(".blue").fadeOut(100).fadeIn(100);
+// 			break;
+// 	}
+
+// 	userClick();
+// }
+
+// function userClick() {
+// 	$(".btn").click(function () {
+// 		var click = $(this).attr("class");
+
+// 		switch (click) {
+// 			case "btn green":
+// 				buttonClicks.push(0);
+// 				break;
+// 			case "btn red":
+// 				buttonClicks.push(1);
+// 				break;
+// 			case "btn yellow":
+// 				buttonClicks.push(2);
+// 				break;
+// 			case "btn blue":
+// 				buttonClicks.push(3);
+// 				break;
+// 		}
+
+// 		if (task == buttonClicks) {
+// 			randomSelector();
+// 		} else {
+// 			console.log("over");
+// 		}
+// 	});
+var buttonColors = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+userClickPattern = [];
+
+
+function nextSequence(){
+	var randomNumber = Math.floor(Math.random() * 4);
+	var randomChosenColor = buttonColors[randomNumber];
+
+	switch (randomChosenColor) {
+				case "green":
+					$(".green").fadeOut(50).fadeIn(50);
+					playSound("green");
+					break;
+				case "red":
+					$(".red").fadeOut(50).fadeIn(50);
+					playSound("red");
+					break;
+				case "yellow":
+					$(".yellow").fadeOut(50).fadeIn(50);
+					playSound("yellow");
+					break;
+				case "blue":
+					$(".blue").fadeOut(50).fadeIn(50);
+					playSound("blue");
+					break;
+			}
 }
 
-function randomSelector() {
-	var random = Math.floor(Math.random() * 4);
-	task.push(random);
+$(".btn").click(function () {
+			var userChosenColor= $(this).attr("id");
+			userClickPattern.push(userChosenColor);
 
-	switch (random) {
-		case 0:
-			$(".green").fadeOut(100).fadeIn(100);
-			break;
-		case 1:
-			$(".red").fadeOut(100).fadeIn(100);
-			break;
-		case 2:
-			$(".yellow").fadeOut(100).fadeIn(100);
-			break;
-		case 3:
-			$(".blue").fadeOut(100).fadeIn(100);
-			break;
-	}
+			animatePress(userChosenColor);
 
-	userClick();
+			switch (userChosenColor) {
+				case "green":
+					playSound("green");
+					break;
+				case "red":
+					playSound("red");
+					break;
+				case "yellow":
+					playSound("yellow");
+					break;
+				case "blue":
+					playSound("blue");
+					break;
+			}
+})
+
+
+function playSound(name){
+	var audio = new Audio("sounds/" + name + ".mp3");
+	audio.play();
 }
 
-function userClick() {
-	$(".btn").click(function () {
-		var click = $(this).attr("class");
+function animatePress(currentColor){
+	$("." + currentColor).addClass("pressed");
 
-		switch (click) {
-			case "btn green":
-				buttonClicks.push(0);
-				break;
-			case "btn red":
-				buttonClicks.push(1);
-				break;
-			case "btn yellow":
-				buttonClicks.push(2);
-				break;
-			case "btn blue":
-				buttonClicks.push(3);
-				break;
-		}
-
-		if (task == buttonClicks) {
-			randomSelector();
-		} else {
-			console.log("over");
-		}
-	});
+	setTimeout(function () {
+		$("." + currentColor).removeClass("pressed");
+	}, 100);
 }
